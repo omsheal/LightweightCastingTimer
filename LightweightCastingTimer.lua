878b79328cccd8cfbaa62ab506ab8740b7f52c03
omsheal <90083715+omsheal@users.noreply.github.com>
2024-08-19 01:05:27 -0700

    Update LightweightCastingTimer.lua

--- LightweightCastingTimer.lua
+++ LightweightCastingTimer.lua
@@ -1,5 +1,5 @@
 -- Author: Omx-Area52
--- Version: 2.2
+-- Version: 2.3
 
 local CastingTimerFormat = "(%0.2f/%0.2fs)";
 local castingTimerFrame = CreateFrame("Frame", "castingTimerFrame", PlayerCastingBarFrame)
